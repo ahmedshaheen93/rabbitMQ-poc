04a72cfab1b4baebb71c58344e1d9d4d5887092f
ahmed shaheen <ahmedshaheen93@yahoo.com>
2023-06-22 01:02:06 +0300

    init poc

---
+++ src/main/java/com/shaheen/rabbitmqpoc/RabbitMqPocApplication.java
@@ -0,0 +1,13 @@
+package com.shaheen.rabbitmqpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqPocApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RabbitMqPocApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/shaheen/rabbitmqpoc/RabbitMqPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaheen.rabbitmqpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqPocApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
